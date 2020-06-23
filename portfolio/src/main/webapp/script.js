1c276a75c7930eb3b2999d48ab5a73aa94063803
Andrew Hojel <alhojelmx@gmail.com>
2020-06-23 18:46:37 +0000

    forgot to add some files to the previous commit -- check the previous commit description

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,19 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFact() {
+  const facts =
+      ['🏙️: I am from Mexico City (capital of Mexico)', '🏎️: I am an avid Formula 1 fan',
+       '🏫: I graduated from highschool a year early', 
+       '🎓: I was at Stanford for a year with both of my older siblings',
+       '🚣‍♂️: I was a competitive rower in high school'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random fact.
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  // Add the fact tp the stage.
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
